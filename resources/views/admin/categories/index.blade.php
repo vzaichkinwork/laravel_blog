6e55c78ee7d7f913f3a35660a5acc7957f56c207
Vitalii Zaichkin <vzaichkinwork@gmail.com>
2019-04-08 15:18:33 +0300

    categories table to fix

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -11,8 +11,34 @@
         <hr>
 
         <a href="{{route('admin.category.create')}}" class="btn btn-primary pull-right">
-            <i class="fa fa-plus-square-o"></i>
+            <i class="fa fa-plus-square-o"> Создать категорию</i>
         </a>
+        <table class="table-striped">
+            <thead>
+            <tr>
+                <th>Наименование</th>
+                <th>Публикация</th>
+                <th class="text-right">Действие</th>
+            </tr>
+            </thead>
+            <tbody>
+                @forelse($categories as $category)
+                    <tr>
+                        <td>{{$category->title}}</td>
+                        <td>{{$category->published}}</td>
+                        <td>
+                            <a href="{{route('admin.category.edit', ['id'=>$category->id])}}">
+                                <i class="fa fa-edit"></i>
+                            </a>
+                        </td>
+                    </tr>
+                @empty
+                    <tr>
+                        <td colspan="3" class="text-center"><h2>Данные отсутствуют</h2></td>
+                    </tr>
+                @endforelse
+            </tbody>
+        </table>
 
     </div>
 @endsection()
